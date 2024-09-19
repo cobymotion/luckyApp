f82280cca4bd769bb5df8139d4fb646efb0cbc24
Luis Cobian <coby10@gmail.com>
2024-09-19 17:33:27 -0600

    Es mi proyecto inicial

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SAVE</title>
+    <!-- Sheet styles -->
+    <link href="helpers/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="helpers/DataTables/datatables.min.css" rel="stylesheet">
+    <link href="helpers/fontawesome-6.4.2-web/css/fontawesome.min.css" rel="stylesheet">
+    <link href="helpers/fontawesome-6.4.2-web/css/brands.min.css" rel="stylesheet">
+    <link href="helpers/fontawesome-6.4.2-web/css/solid.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+    <!-- End Sheet styles -->
+</head>
+<body>
+    <div class="container-xxl">
+            <div id="body">
+                <div class="row">
+                    <h1>Mi proyecto</h1>
+                </div>
+            </div>
+    </div>
+    <!-- Scripts -->
+        <script src="helpers/jquery-3.7/jquery-3.7.1.min.js"></script>
+        <script src="helpers/popper-2.11.8/popper.min.js"></script>
+        <script src="helpers/moment-18.1/moment.min.js"></script>
+        <script src="helpers/bootstrap-5.0.2-dist/js/bootstrap.bundle.js"></script>
+        <script src="helpers/DataTables/datatables.min.css" ></script>
+    <!-- Ends  Scripts -->
+</body>
+</html>
